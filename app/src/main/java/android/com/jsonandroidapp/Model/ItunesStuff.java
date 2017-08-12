0d4f4cb05f697113817311310e7b321b9b24236c
Dexter John Datul <ddatul@almadaclinic.com>
2017-08-12 19:51:12 +0300

    Simple JSON Response Android App

---
+++ app/src/main/java/android/com/jsonandroidapp/Model/ItunesStuff.java
@@ -0,0 +1,59 @@
+package android.com.jsonandroidapp.Model;
+
+/**
+ * Created by Dexter John Datul on 12/08/2017.
+ */
+
+public class ItunesStuff {
+
+    String type, kind, artistName, collectionName, trackName, artistViewURL;
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getArtistViewURL() {
+        return artistViewURL;
+    }
+
+    public void setArtistViewURL(String artistViewURL) {
+        this.artistViewURL = artistViewURL;
+    }
+}
